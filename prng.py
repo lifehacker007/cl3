aea2a5d28181841057531d485be509ce7e8074fb
lifehacker007 <37249172+lifehacker007@users.noreply.github.com>
2018-05-01 13:18:08 +0530

    Create prng.py
    This Will Generate Random Number using the Formula

---
+++ prng.py
@@ -0,0 +1,18 @@
+#PseduRandom Number Generator Program
+seed = 123456789 
+
+a = 1103515245
+c = 12345
+m = pow(2,32)
+
+def LCG() :
+    global seed
+    seed = (a * seed + c) % m
+    return seed
+
+print("How many random numbers do you want ? ")
+n = int(raw_input())
+i = 0
+while i<n :
+    print(LCG())
+    i = i + 1
